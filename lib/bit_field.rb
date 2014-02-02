7063705a34df99a73d089631f92a58f0dcced9c5
Andre Henry <ahenry@thinkthroughmath.com>
2014-02-02 18:16:18 -0500

    Bit field from array and string

---
+++ lib/bit_field.rb
@@ -0,0 +1,24 @@
+require 'ostruct'
+require_relative 'convertible_field'
+
+module BitFields
+  class BitField
+    attr_reader :bit_field
+
+    def initialize(structure, data)
+      @bit_field = OpenStruct.new(structure)
+      @index = 0
+
+      structure.each_key{ |key|
+        field_data = data[@index, structure[key]]
+        @bit_field.send("#{key}=".to_sym, field_data)
+        @index += structure[key]
+      }
+    end
+
+    def self.from_string(structure, data)
+      tmp = data.split('').map { |i| i.to_i }
+      BitField.new(structure, tmp)
+    end
+  end
+end
---
+++ spec/server/bit_field_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path('lib/bit_field.rb')
+require 'minitest/autorun'
+
+describe BitFields::BitField do
+
+  before do
+    @data = [1,1,1,0,0,1,0,1]
+    @string_data = '11100101'
+  end
+
+  it 'should initialize from array' do
+    structure = {:f1 => 1, :f2 => 3, :f3 => 4}
+    bf = BitFields::BitField.new(structure, @data)
+
+    bf.bit_field.f1.must_equal [1]
+    bf.bit_field.f2.must_equal [1,1,0]
+    bf.bit_field.f3.must_equal [0,1,0,1]
+  end
+
+  it 'should handle edge cases' do
+    structure = {:f1 => 3, :f2 => 4, :f3 => 1}
+    bf = BitFields::BitField.new(structure, @data)
+
+    bf.bit_field.f1.must_equal [1,1,1]
+    bf.bit_field.f3.must_equal [1]
+  end
+
+  it 'should initialize from string' do
+    structure = {:f1 => 1, :f2 => 3, :f3 => 4}
+    bf = BitFields::BitField.from_string(structure, @string_data)
+
+    bf.bit_field.f1.must_equal [1]
+    bf.bit_field.f2.must_equal [1,1,0]
+    bf.bit_field.f3.must_equal [0,1,0,1]
+  end
+
+end
